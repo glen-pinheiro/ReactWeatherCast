1025316ef053e788c654b8a0bc4ada1fa2b3b75b
Glen Pinheiro <glen.pinheiro@gmail.com>
2016-10-22 11:20:51 +0530

    Apply foundation and jQuery.

--- app/app.jsx
+++ app/app.jsx
@@ -7,6 +7,10 @@ import Weather from 'Weather';
 import About from 'About';
 import Examples from 'Examples';
 
+// Foundation styles.
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
